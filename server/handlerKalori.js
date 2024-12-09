1a119135ab24c8e46f88bdeeff88cab4d3ca7297
Vicky Yohanes Putra Setiawan / 71220956 <vicky.yohanes@ti.ukdw.ac.id>
2024-12-09 11:15:18 +0700

    Push kalori harian

--- server/handlerKalori.js
+++ server/handlerKalori.js
@@ -85,3 +85,80 @@ export const getKalori = async (request,response) => {
         });
     }
 }
+
+export const getKaloriHarian = async (request,response) => {
+
+    try {
+        const user_id = request.params.user_id;
+        const pool = await initPool();
+        const conn = await pool.getConnection();
+        const [query] = await conn.query(`SELECT nama_lengkap_user,kalori_harian FROM detail_user WHERE user_id = ${user_id};`);
+        conn.release();
+        if (query.length == 0) {
+            return response.status(404).json({
+                statusCode: 404,
+                error: true,
+                message: 'fail',
+                describe: 'data not found'
+            });
+        }else{
+            return response.status(200).json({
+                statusCode: 200,
+                error: false,
+                message: 'success',
+                data: query[0]
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({
+            statusCode: 500,
+            error: true,
+            message: 'Internal Server Error',
+        });
+    }
+}
+
+export const updateKaloriHarian = async (request, response) => {
+    
+    try {
+        const user_id = request.params.user_id;
+        const {kaloriAdd} = request.body;
+        if (!kaloriAdd) {
+            return response.status(400).json({
+                statusCode: 400,
+                error: true,
+                message: 'fail',
+                describe: 'check your format'
+            });
+        }
+        const pool = await initPool();
+        const conn = await pool.getConnection();
+        const [query] = await conn.query(`UPDATE detail_user SET kalori_harian = kalori_harian + ? WHERE user_id = ?;`,[kaloriAdd, user_id]);
+        conn.release();
+        if (query.affectedRows == 0) {
+            return response.status(404).json({
+                statusCode: 404,
+                error: true,
+                message: 'fail',
+                describe: 'data not found'
+            });
+        }else{
+            return response.status(200).json({
+                statusCode: 200,
+                error: false,
+                message: 'success',
+                describe: 'update data berhasil'
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({
+            statusCode: 500,
+            error: true,
+            message: 'Internal Server Error',
+        });
+    }
+
+}
--- server/kalori.js
+++ server/kalori.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { updateKalori, getKalori } from './handlerKalori.js';
+import { updateKalori, getKalori, getKaloriHarian, updateKaloriHarian } from './handlerKalori.js';
 const router4 = express.Router();
 
 router4.put('/updateKalori/:user_id',updateKalori);
 router4.get('/getKalori/:user_id', getKalori);
+router4.get('/getKaloriHarian/:user_id', getKaloriHarian);
+router4.put('/updateKaloriHarian/:user_id', updateKaloriHarian);
 
 export default router4;
