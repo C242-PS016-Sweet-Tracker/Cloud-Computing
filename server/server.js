63e8d8d606fa16f003a4fbeece8e58ccc63e5548
gio <stevianohp@gmail.com>
2024-12-09 20:42:11 +0700

    ignore credentials

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use('/detail', router2);
 app.use('/profil', router3);
 app.use('/kalori', router4);
 
+//Ini 1 menit, belum diganti ke 24 jam
+// cron.schedule('* * * * *', async () => {
+//   try {
+//     await delete24jamCron();  // Panggil fungsi delete24jamCron untuk reset kalori_harian
+//     console.log('Kalori harian berhasil direset pada menit ini.');
+//   } catch (error) {
+//     console.error('Terjadi kesalahan saat mereset kalori harian di cron job:', error);
+//   }
+// });
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
